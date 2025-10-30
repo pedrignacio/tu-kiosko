38d94f7f0c5bd6dbccd29ca7cffd2eed7ded629f
pedro ignacio <ped.sanmartin@duocuc.cl>
2025-10-30 16:06:16 -0300

    feat: nueva funcionalidad

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tu-kiosko/', // ← Debe estar exactamente así
+  base: '/tu-kiosko/', // ← DEBE estar así
   server: {
     port: 3000
   },
